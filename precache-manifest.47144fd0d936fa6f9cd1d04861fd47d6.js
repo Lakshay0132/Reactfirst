546eeda20e0f5b3d9283c4702911c7d3f2598175
lakshay0132 <lakshay0132@gmail.com>
2020-09-05 17:07:19 +0530

    Updates

---
+++ precache-manifest.47144fd0d936fa6f9cd1d04861fd47d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140e049bb6f07c64cc99b643b4713dd9",
+    "url": "/Reactfirst/index.html"
+  },
+  {
+    "revision": "3dde14146c4a022fdef7",
+    "url": "/Reactfirst/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7922b88095ad3362e9be",
+    "url": "/Reactfirst/static/css/main.9349fa58.chunk.css"
+  },
+  {
+    "revision": "3dde14146c4a022fdef7",
+    "url": "/Reactfirst/static/js/2.68ee97d5.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "/Reactfirst/static/js/2.68ee97d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7922b88095ad3362e9be",
+    "url": "/Reactfirst/static/js/main.09ffc9f7.chunk.js"
+  },
+  {
+    "revision": "46ca09204446e57b1b7b",
+    "url": "/Reactfirst/static/js/runtime-main.58ed5477.js"
+  }
+]);
